be3194d4e2a615dd2ae594ca330cc5edf4c45e49
jihadanbs <jihadanbs11@gmail.com>
2024-08-07 18:52:57 +0700

    push routes, controllers

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/home');
 });
+
+// Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::post('/store', 'HomeController@store')->name('store');
+Route::get('/api/province/{id}/cities', 'HomeController@getCities');
+Route::post('/api/cities', 'HomeController@searchCities');
